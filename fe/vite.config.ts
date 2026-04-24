3f6e863fc9c3602303da6437401d33c654bcf255
zhuyuqiao <zhuyuqiao@xiaohongshu.net>
2026-04-24 17:06:17 +0800

    feat: 前端code-inspector-plugin插件

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { codeInspectorPlugin } from 'code-inspector-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    codeInspectorPlugin({
+      bundler: 'vite',
+    }),
+    vue(),
+  ],
   server: {
     proxy: {
       '/api': {
